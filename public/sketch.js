29f508ae1c12654021fa70cb36f9622489f7ca54
karanbhagat <cv.ryn7@gmail.com>
2017-07-13 23:30:42 -0400

    some improvements

--- public/sketch.js
+++ public/sketch.js
@@ -12,8 +12,8 @@ function setup() {
         blobs[i] = new Blob(x, y, 16, random(255), random(255), random(255));
     }
     for (var i = 0; i < 500; i++) {
-        var x = random(-width * 5, width * 5);
-        var y = random(-height * 5, height * 5);
+        var x = random(-width * 3, width * 3);
+        var y = random(-height * 3, height * 3);
         stars[i] = new Star(x, y, 2);
     }
 
