0ba6c430fcd62295872b08b94fb6d931bc972e87
karanbhagat <cv.ryn7@gmail.com>
2017-07-13 23:28:49 -0400

    Now star twinkle

--- public/star.js
+++ public/star.js
@@ -2,11 +2,12 @@
 function Star(x, y, radius) {
     this.pos = createVector(x, y);
     this.radius = radius; // radius of star
-    var r = random(255);
-    var g = random(255);
-    var b = random(255);
+
     //Render the star
     this.show = () => {
+        var r = random(255);
+        var g = random(255);
+        var b = random(255);
         fill(r,g,b); // star color
         ellipse(this.pos.x, this.pos.y, this.radius * 2, this.radius * 2);
     }
